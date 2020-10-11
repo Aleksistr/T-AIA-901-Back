b049caed18626857c8235d8a2e3a07b245752db6
Brice Tossim <brice@comptoirducode.com>
2020-10-11 20:22:54 +0200

    update

--- src/controllers/TtsController.js
+++ src/controllers/TtsController.js
@@ -4,6 +4,7 @@ const textToSpeech = require('@google-cloud/text-to-speech');
 // Import other required libraries
 const fs = require('fs');
 const util = require('util');
+
 // Creates a client
 const client = new textToSpeech.TextToSpeechClient();
 
