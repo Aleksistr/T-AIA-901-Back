faedc9ab89a90a83ab78ecde6f5380a6b11308d0
Brice Tossim <brice@comptoirducode.com>
2020-10-18 23:27:57 +0200

    update port

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ app.use('/api/', routes)
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(8080, () => {
+app.listen(3000, () => {
     console.log("Serveur à l'écoute")
 })
