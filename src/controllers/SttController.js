ffe2b5e88f9320d4f0634960a081d917b4440a35
Brice Tossim <brice@comptoirducode.com>
2020-11-01 21:58:20 +0100

    log text transcripted

--- src/controllers/SttController.js
+++ src/controllers/SttController.js
@@ -46,6 +46,7 @@ const transcriptByAudio = async (req, res) => {
 
     // Post to python text converted and return response to front
     // ...soon
+    console.log(transcription)
 
     // Return response
     return res.status(statusCode.OK)
